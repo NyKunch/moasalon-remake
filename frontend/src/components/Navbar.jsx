49250df2b35c8f94ea878570dd63cc169ce03b26
NyKunch <ngakanputudoni@gmail.com>
2025-04-04 22:22:29 +0800

    Feat: navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className="w-full py-4 mt-5 flex justify-center items-center">
+      <h1 className="text-5xl font-bold bg-gradient-to-r from-orange-300 to-amber-400 bg-clip-text 
+      text-transparent">
+        Moa Salon
+      </h1>
+    </div>
+  )
+}
+
+export default Navbar
