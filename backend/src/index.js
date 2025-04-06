b18a9b647fafedfb76ad077bc0eceb90f3ed14d7
NyKunch <ngakanputudoni@gmail.com>
2025-04-06 15:14:59 +0800

    Dep: cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import { connectDB } from './lib/db.js'
 import authRoute from './routers/auth.route.js'
@@ -11,7 +13,13 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}))
+
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth', authRoute)
 app.use('/api/staff', staffRoute)
